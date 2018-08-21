3a1eeafc5914c7cfda5c63a8de4780acc9b59471
Jason Whitehorn <jason.whitehorn@gmail.com>
2018-08-20 23:05:00 -0500

    point to version 0.1 of html2epub

--- html2epub.rb
+++ html2epub.rb
@@ -1,8 +1,8 @@
 class Html2epub < Formula
   desc "html2epub"
   homepage "https://github.com/jwhitehorn/html2epub"
-  url "https://github.com/jwhitehorn/html2epub/archive/b2f9b6c2b4e2c2120c84e7f58e7c54dc0a629ad8.tar.gz"
-  sha256 "8c563681c41ddc801a9970a4745c58252afeb3645d511e71e2ce98fc3ebc4afd"
+  url "https://github.com/jwhitehorn/html2epub/archive/0.1.tar.gz"
+  sha256 "f77d8887287d7ef5551210d677a703b3157cb810eb66344707b2f13ebff48079"
 
   depends_on "ruby" => :build
 
